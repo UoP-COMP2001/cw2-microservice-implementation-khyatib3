58293bac9c97b6e49d2864da5d3fe4fed190b376
khyat <khyatib@live.co.uk>
2026-01-13 13:57:25 +0000

    Modified the authenticator

--- authenticator.py
+++ authenticator.py
@@ -5,8 +5,6 @@ def user_authenticator(email, password, required_scope = None, request = None):
     print(f"Authenticating user with email: {email} and password: {password}") # print log
     request = requests.post('https://web.socem.plymouth.ac.uk/COMP2001/auth/api/users',json={'email': email, 'password': password}) # creating request
 
-    if request.status_code == 200:
-        if request.text.find("True") != 1:
-            return {'email': email, 'password': password} # return the user creds if the authentication was successful
-        return None
+    if request.ok and request.text.find("True") != -1:
+        return {"validated email": email, "validated password": password} # return validated email and password if ok
     return None
