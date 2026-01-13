14bbe08c2dbbf39f5cdf9d9e062e0c88a833671a
khyat <khyatib@live.co.uk>
2026-01-13 11:42:48 +0000

    Added validation schemas for user account creation, display and update.

---
+++ schemas/schemas.py
@@ -0,0 +1,75 @@
+from marshmallow import fields, Schema, validate
+from models import Users, UserActivity, Activity, UserSavedTrails
+
+class DisplayAccountSchema(Schema):
+    # all fields to show when showing user account info
+    username = fields.Str(dump_only=True)
+    email = fields.Str(dump_only=True)
+    phone_no = fields.Int(dump_only=True)
+    first_name = fields.Str(dump_only=True)
+    last_name = fields.Str(dump_only=True)
+    dob = fields.Date(dump_only=True)
+    height = fields.Decimal(dump_only=True)
+    weight = fields.Decimal(dump_only=True)
+    about_me = fields.Str(dump_only=True)
+    marketing_language = fields.Str(dump_only=True)
+    preferred_unit_metric = fields.Str(dump_only=True)
+    time_preference_speed = fields.Str(dump_only=True)
+    
+    # use methods to get the actual name/list instead of ids
+    location = fields.Method("retrieve_location", dump_only=True)
+    activities_list = fields.Method("retrieve_activities", dump_only=True)
+    saved_trails = fields.Method("retrieve_saved_trails", dump_only=True)
+    role_type = fields.Method("retrieve_role", dump_only=True)
+
+    def retrieve_location(self, obj):
+        if obj.location:
+            return obj.location.location
+        return None
+
+    def retrieve_activities(self, obj):
+        activities = [] # activities list
+        for activity in obj.activities_list:
+            act = Activity.query.get(activity.activityID)
+            if act:
+                activities.append(act.activity_name)
+        return activities
+
+    def retrieve_saved_trails(self, obj):
+        trails = []
+        for trail in obj.trails_list:
+            trails.append({
+                'trailID': trail.trailID, # have to return trail id as no trail name
+                'trail_saved_timestamp': trail.trail_saved_timestamp # show timestamp for each id too
+            })
+        return trails
+
+    def retrieve_role(self, obj):
+        if obj.role_type:
+            return obj.role_type.role_name # for testing to see differentiation in roles
+        return None
+
+display_users_schema = DisplayAccountSchema(many=True) # for many users
+display_user_schema = DisplayAccountSchema() # for a single user
+
+class CreateAccountSchema(Schema):
+    username = fields.Str(required=True, validate=validate.Length(min=4, max=40))
+    password = fields.Str(required=True, validate=validate.Length(min=6, max=128))
+    email = fields.Str(validate=validate.Length(max=60))
+create_account_schema = CreateAccountSchema()
+
+class UpdateAccountSchema(Schema):
+    username = fields.Str(validate=validate.Length(min=4, max=40))
+    password = fields.Str(validate=validate.Length(min=6, max=128))
+    email = fields.Str(validate=validate.Length(max=60))
+    phone_no = fields.Int()
+    first_name = fields.Str()
+    last_name = fields.Str()
+    dob = fields.Date()
+    height = fields.Decimal()
+    weight = fields.Decimal()
+    about_me = fields.Str(validate=validate.Length(max=700))
+    marketing_language = fields.Str(validate=validate.Length(max=50))
+    preferred_unit_metric = fields.Str(validate=validate.Length(max=10))
+    time_preference_speed = fields.Str(validate=validate.Length(max=10))
+update_account_schema = UpdateAccountSchema()             
