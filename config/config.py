2fba3c922f11933a62e95515bd53505555e7d3fa
khyat <khyatib@live.co.uk>
2026-01-14 18:17:57 +0000

    changed config content to enable swagger ui

--- config/config.py
+++ config/config.py
@@ -5,26 +5,29 @@ from flask_marshmallow import Marshmallow
 
 basedir = pathlib.Path(__file__).parent.resolve()
 
+# Enable Swagger UI explicitly
+from connexion.options import SwaggerUIOptions
+swagger_ui_options = SwaggerUIOptions(swagger_ui=True, swagger_ui_path="/ui")
+connex_app = connexion.App(__name__, specification_dir=basedir.parent, swagger_ui_options=swagger_ui_options)
+
+app = connex_app.app
+
+# Configure template folder to point to project root templates directory
+template_path = basedir.parent / "templates"
+app.template_folder = str(template_path.absolute())
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mssql+pyodbc:///?odbc_connect="
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=dist-6-505.uopnet.plymouth.ac.uk;"
+    "DATABASE=COMP2001_KBhatnagar;"
+    "UID=KBhatnagar;"
+    "PWD=ArlH525;"
+    "TrustServerCertificate=yes;"
+    "Encrypt=yes;"
+    )
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 db = SQLAlchemy()
 ma = Marshmallow()
-
-def create_connexion_app():
-    connex_app = connexion.App(__name__, specification_dir=basedir)
-    flask_app = connex_app.app
-
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "mssql+pyodbc:///?odbc_connect="
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=dist-6-505.uopnet.plymouth.ac.uk;"
-        "DATABASE=COMP2001_KBhatnagar;"
-        "UID=KBhatnagar;"
-        "PWD=ArlH525;"
-        "TrustServerCertificate=yes;"
-        "Encrypt=yes;"
-    )
-    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.init_app(flask_app)
-    ma.init_app(flask_app)
-
-    return connex_app
